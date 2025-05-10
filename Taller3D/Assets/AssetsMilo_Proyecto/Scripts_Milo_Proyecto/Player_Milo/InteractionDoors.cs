92bb82e9e9dbb363591aa2c718cd577fbcb9b293
smol4050 <smolstiv4050@gmail.com>
2025-05-10 09:58:46 -0500

    milo

--- Taller3D/Assets/AssetsMilo_Proyecto/Scripts_Milo_Proyecto/Player_Milo/InteractionDoors.cs
+++ Taller3D/Assets/AssetsMilo_Proyecto/Scripts_Milo_Proyecto/Player_Milo/InteractionDoors.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public class InteractionDoors : MonoBehaviour, IInteractuable
 {
+    [SerializeField]
+    private Animator doorAnimator;
+
+    [SerializeField]
+    private GameController controller;
+
+    [SerializeField]
+    private bool isMechanism = true;
+
+    private bool used = false;
+
+    
+    void OnTriggerStay(Collider other)
+    {
+        
+        if (!used && other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        {
+            ActivarObjeto();
+        }
+    }
+
+    
     public void ActivarObjeto()
     {
-        Destroy(gameObject);
+        used = true;
+
+        
+        if (isMechanism)
+            controller.OnMechanismActivated();
+
+       
+        if (doorAnimator != null)
+            doorAnimator.SetTrigger("Open");
     }
 }
+
